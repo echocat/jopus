1009300fe6129bbfbea321dde30c320a614b02d6
Christian Rijke <christian.rijke@teufel.de>
2014-08-26 11:50:44 +0200

    Change input stream iteration to also read in last buffer (which might be smaller than regular read buffers).

--- src/test/java/org/echocat/jopus/OpusEncoderTest.java
+++ src/test/java/org/echocat/jopus/OpusEncoderTest.java
@@ -15,51 +15,80 @@
 package org.echocat.jopus;
 
 import org.echocat.jogg.OggSyncStateOutput;
-import org.echocat.jopus.OpusComments;
-import org.echocat.jopus.OpusEncoder;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by christian.rijke on 14.08.2014.
  */
 public class OpusEncoderTest {
 
+    Logger LOG = LoggerFactory.getLogger(OpusEncoderTest.class);
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
     @Test
-    public void encodeRawFileToOpusFile() throws IOException {
+    public void encodeMono() throws IOException {
+        final OpusComments commments = new OpusComments(
+            "ARTIST", "J.S. Bach",
+            "GENRE", "Classical",
+            "TRACKNUMBER", "1"
+        );
+        File result = encode("bach_48k_mono.wav", commments, 1, SamplingRate.kHz48);
+        assertThat(result.length(), is(73898L));
+    }
+
+    @Test
+    public void encodeStereo() throws IOException {
+        final OpusComments commments = new OpusComments(
+            "ARTIST", "J.S. Bach",
+            "GENRE", "Classical",
+            "TRACKNUMBER", "1"
+        );
+        File result = encode("bach_48k_stereo.wav", commments, 2, SamplingRate.kHz48);
+        assertThat(result.length(), is(140784L));
+    }
+
+    public File encode(String source, OpusComments opusComments, int channels, SamplingRate samplingRate) throws IOException {
+        LOG.info("encoding {}...", source);
+
         final int numberOfFrames = 2880;
 
-        try (final InputStream is = new FileInputStream("foo.raw");
+        File tempFile = tempFolder.newFile(source.substring(0, source.length() - 4) + ".opus");
+
+        try (final InputStream is = getClass().getResourceAsStream(source);
              final DataInputStream in = new DataInputStream(is);
-             final OutputStream out = new FileOutputStream("foo.opus");
+             final OutputStream out = new FileOutputStream(tempFile);
              final OggSyncStateOutput sso = new OggSyncStateOutput(out);
              final OpusEncoder oe = new OpusEncoder(sso)) {
 
-            final OpusComments opusComments = new OpusComments(
-                "ALBUM", "Foo",
-                "ARTIST", "deadmau5",
-                "DATE", "2012",
-                "ENCODER", "opusenc from opus-tools 0.1.8",
-                "GENRE", "Electronic",
-                "TITLE", "Strobe",
-                "TRACKNUMBER", "1"
-            );
             oe.setComments(opusComments);
+            oe.setNumberOfChannels(channels);
+            oe.setSamplingRate(samplingRate);
 
             final short[] pcm = new short[numberOfFrames * oe.getNumberOfChannels()];
             final byte[] buffer = new byte[pcm.length * 2];
-            try {
-                //noinspection InfiniteLoopStatement
-                for (; ; ) {
-                    in.readFully(buffer);
-                    for (int i = 0; i < pcm.length; i++) {
-                        pcm[i] = (short) ((buffer[i * 2] & 0xff) | (buffer[i * 2 + 1] << 8));
-                    }
-                    oe.write(numberOfFrames, pcm);
+            int numberOfBytes;
+            while ((numberOfBytes = in.read(buffer)) != -1) {
+                int numberOfShorts = numberOfBytes / 2;
+                for (int i = 0; i < numberOfShorts; i++) {
+                    pcm[i] = (short) ((buffer[i * 2] & 0xff) | (buffer[i * 2 + 1] << 8));
                 }
-            } catch (final EOFException ignored) {
+                oe.write(numberOfFrames, Arrays.copyOfRange(pcm, 0, numberOfShorts));
             }
+
         }
+        LOG.info("encoding finished");
+        return tempFile;
     }
 }
